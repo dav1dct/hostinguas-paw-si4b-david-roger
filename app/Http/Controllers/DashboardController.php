e71f8144b579c44f14c4bbbcf35190c2ba4d6af8
dav1dct <lindahwatih215@gmail.com>
2024-07-05 19:27:40 +0700

    a

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Barang;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $barang = Barang::all();
+        $stokCategories = $barang->pluck('nama')->toArray();
+        $stokBarangs = $barang->pluck('stock')->toArray();
+        return view('dashboard.dashboard', compact('stokCategories', 'stokBarangs'));
+    }
+}
